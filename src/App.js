b4510160b75ab01f5b81c177873ee728f2a0caa9
anjishnucode-debug <anjishnucode@gmail.com>
2026-05-23 11:24:09 +0530

    feat: dark/light theme toggle

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
+import Landing from './pages/Landing';
+import CitizenHome from './pages/citizen/CitizenHome';
+import StatusTracker from './pages/citizen/StatusTracker';
+import RightsBot from './pages/citizen/RightsBot';
+import LegalAidFinder from './pages/citizen/LegalAidFinder';
+import CourtCalendar from './pages/citizen/CourtCalendar';
+import LegalHome from './pages/legal/LegalHome';
+import NyayMitra from './pages/legal/NyayMitra';
+import PrecedentFinder from './pages/legal/PrecedentFinder';
+import UndertrialAlerts from './pages/legal/UndertrialAlerts';
+import BailGenerator from './pages/legal/BailGenerator';
+import NotFound from './pages/NotFound';
+import './App.css';
+import './index.css';
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/citizen" element={<CitizenHome />} />
+          <Route path="/citizen/status" element={<StatusTracker />} />
+          <Route path="/citizen/rights" element={<RightsBot />} />
+          <Route path="/citizen/legal-aid" element={<LegalAidFinder />} />
+          <Route path="/citizen/calendar" element={<CourtCalendar />} />
+          <Route path="/legal" element={<LegalHome />} />
+          <Route path="/legal/nyay-mitra" element={<NyayMitra />} />
+          <Route path="/legal/precedents" element={<PrecedentFinder />} />
+          <Route path="/legal/alerts" element={<UndertrialAlerts />} />
+          <Route path="/legal/bail" element={<BailGenerator />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
